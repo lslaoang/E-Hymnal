097980712d2523d97814c65d2ed8f7eb423dcd6b
Laurel Lao Ang <laurellaoang@gmail.com>
2022-01-29 20:53:17 +0800

    Initial commit

---
+++ src/main/java/com/example/ehymnal/EHymnalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehymnal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHymnalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHymnalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ehymnal/EHymnalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehymnal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHymnalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
