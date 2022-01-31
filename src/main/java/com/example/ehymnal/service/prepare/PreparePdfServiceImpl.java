efe26c1100aaf81a7492b523192d6bfd8adde669
Laurel Lao Ang <laurellaoang@gmail.com>
2022-01-31 10:01:46 +0800

    Created blank implementation

---
+++ src/main/java/com/example/ehymnal/service/prepare/PreparePdfServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.ehymnal.service.prepare;
+
+public class PreparePdfServiceImpl implements PreparePdfService {
+    @Override
+    public void splitPdfFilesByPage(String pdfFileAsString, int page) {
+
+    }
+
+    @Override
+    public void splitPdfFileIndividually(String pdfFileAsString) {
+
+    }
+
+    @Override
+    public void collatePdfFiles() {
+
+    }
+
+    @Override
+    public void updatePdfFile() {
+
+    }
+}
