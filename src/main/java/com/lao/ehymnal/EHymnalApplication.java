b6443c4537a8dc4991fb3e9b0c458e0628d419e9
Laurel Lao Ang <laurellaoang@gmail.com>
2022-02-17 14:23:23 +0800

    Added actuator Closed #19

--- src/main/java/com/lao/ehymnal/EHymnalApplication.java
+++ src/main/java/com/lao/ehymnal/EHymnalApplication.java
@@ -2,8 +2,10 @@ package com.lao.ehymnal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.lao.ehymnal.repository")
 public class EHymnalApplication {
 
     public static void main(String[] args) {
