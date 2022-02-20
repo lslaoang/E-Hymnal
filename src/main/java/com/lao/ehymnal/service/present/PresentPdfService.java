146c97297bd38c5f395183bcfc20a20a82ad05af
Laurel Lao Ang <laurellaoang@gmail.com>
2022-02-20 15:03:24 +0800

    Created sorting implementation

--- src/main/java/com/lao/ehymnal/service/present/PresentPdfService.java
+++ src/main/java/com/lao/ehymnal/service/present/PresentPdfService.java
@@ -1,7 +1,19 @@
 package com.lao.ehymnal.service.present;
 
+import com.lao.ehymnal.model.Hymn;
+
+import java.util.List;
+
 public interface PresentPdfService {
 
     void displayPdfFiles();
+
+    List<Hymn> sortHymnById();
+
+    List<Hymn> sortHymnByLanguage();
+
+    List<Hymn> sortHymnByPrintDate();
+
+    List<Hymn> sortHymnByTitle();
 
 }
--- src/main/java/com/lao/ehymnal/service/present/PresentPdfServiceImpl.java
+++ src/main/java/com/lao/ehymnal/service/present/PresentPdfServiceImpl.java
@@ -1,8 +1,13 @@
 package com.lao.ehymnal.service.present;
 
+import com.lao.ehymnal.model.Hymn;
 import com.lao.ehymnal.repository.HymnRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+
+@SuppressWarnings("unchecked")
 @Service
 public class PresentPdfServiceImpl implements PresentPdfService {
 
@@ -17,4 +22,33 @@ public class PresentPdfServiceImpl implements PresentPdfService {
 
         hymnRepository.findAll();
     }
+
+    @Override
+    public List<Hymn> sortHymnById() {
+        List<Hymn> hymnList = (List<Hymn>) hymnRepository.findAll();
+        hymnList.sort(Comparator.comparing(Hymn::getId));
+        return hymnList;
+    }
+
+    @Override
+    public List<Hymn> sortHymnByLanguage() {
+        List<Hymn> hymnList = (List<Hymn>) hymnRepository.findAll();
+        hymnList.sort(Comparator.comparing(Hymn::getLanguage));
+        return hymnList;
+    }
+
+    @Override
+    public List<Hymn> sortHymnByPrintDate() {
+        List<Hymn> hymnList = (List<Hymn>) hymnRepository.findAll();
+        hymnList.sort(Comparator.comparing(Hymn::getPrintDate));
+        return hymnList;
+    }
+
+    @Override
+    public List<Hymn> sortHymnByTitle() {
+        List<Hymn> hymnList = (List<Hymn>) hymnRepository.findAll();
+        hymnList.sort(Comparator.comparing(Hymn::getTitle));
+        return hymnList;
+    }
+
 }
