08d7f8bcffed81050e211517b057059819dff519
Laurel Lao Ang <laurellaoang@gmail.com>
2022-02-21 10:27:10 +0800

    Additional Module created

---
+++ hymnbook/src/main/java/com/lao/hymnbook/HymnbookApplication.java
@@ -0,0 +1,13 @@
+package com.lao.hymnbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HymnbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HymnbookApplication.class, args);
+    }
+
+}
---
+++ hymnbook/src/test/java/com/lao/hymnbook/HymnbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lao.hymnbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HymnbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
