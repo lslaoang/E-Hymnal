cbd8ef89cf7a51af4ba667f1e53f4acd12c3624d
Laurel Lao Ang <laurellaoang@gmail.com>
2022-10-23 23:32:32 +0800

    add default landing page

---
+++ hymnbook/src/main/java/com/lao/hymnbook/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.lao.hymnbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping(path = {"","/index","index.html"})
+    public String defaultLandingPage(){
+        return "index";
+    }
+}
